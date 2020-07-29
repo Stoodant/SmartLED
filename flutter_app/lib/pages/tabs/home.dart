94bdd4023d3ef99d719a9cd38fb3dd1da0afbfb7
zzzwai <1149941442@qq.com>
2020-07-29 22:17:04 +0800

    update

--- flutter_app/lib/pages/tabs/home.dart
+++ flutter_app/lib/pages/tabs/home.dart
@@ -16,7 +16,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   double _treeWidth = 200.0; //矩形宽度
   double _treeHeight = 300.0; //矩形高度
-  var _contents = [];
+  List<String> _contents = [];
   List _nameList = [];
 
   List<Widget> _getData(List list) {
@@ -46,45 +46,62 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<String> _read(String str, double treeWidth, double treeHeight) async {
-    try {
-      File file = await _getLocalFile(str);
-      String tmp = await file.readAsString();
-      return tmp;
-    } on FileSystemException {
-      print('no data');
+    if (str.isNotEmpty) {
+      try {
+        File file = await _getLocalFile(str);
+        String tmp = await file.readAsString();
+        return tmp;
+      } on FileSystemException {
+        print('no data');
+      }
     }
   }
 
   Future<List> _getName() async {
     File file = await _getLocalFile("nameData");
     String tmp = await file.readAsString();
-    print("this is nameData");
-    print(tmp);
     List<String> res = tmp.split("@");
-    print("this is res");
-    print(res);
-    setState(() {
-      _nameList = res;
-    });
-    
+    for (var i = 0; i < res.length; i++) {
+      if (res[i] == "") {
+        res.remove(res[i]);
+      }
+    }
     return res;
+  }
+
+  void printTest(String s) {
+    // print("this is the $s's test");
+    // print("this is nameList");
+    // print(_nameList);
+
+    // print("this is contents");
+    // print(_contents);
   }
 
   @override
   void initState() {
     // TODO: implement initState
-    setState(() {
-      for (var i = 0; i < _nameList.length; i++) {
-        _read(_nameList[i], _treeWidth, _treeHeight)
-            .then((value) => setState(() => _contents[i] = value));
-      }
-
-      _getName().then((value) => _nameList = value);
+    _getName().then((value) {
+      print("getName success!!!!!!!!!!!!!");
+      _nameList = value;
       print("this is nameList");
       print(_nameList);
-
-      print("this is contents");
-      print(_contents);
+      print(_nameList.length);
+      print("over------------");
+      for (var i = 0; i < _nameList.length; i++) {
+        _read(_nameList[i], _treeWidth, _treeHeight).then((value) {
+          // print(value is String);
+          // print(value);
+          if (value == null) {
+            print("get null value!!");
+          } else {
+            print("nameList success!!!!!!!!!!!!!");
+            _contents.add(value);
+          }
+        });
+      }
+      //print(_contents);
+      setState(() {});
     });
     super.initState();
   }
@@ -108,45 +125,38 @@ class _HomePageState extends State<HomePage> {
             border: Border.all(color: Colors.blueAccent, width: 1)),
       ));
     }
-
-    // for (var i = 0; i < _nameList.length; i++) {
-    //     _read(_nameList[i], _treeWidth, _treeHeight)
-    //         .then((value) => setState(() => _contents[i] = value));
-    //   }
-
-    //   _getName().then((value) => _nameList = value);
-
-    //_read(conList, treeWidth, treeHeight);
-    print(_contents is List);
-    for (var i = 0; i < _contents.length; i++) {
-      //print(contents[i] is String);
-      if (_contents[i].length > 0) {
-        List str = json.decode(_contents[i]);
-        List<Widget> tmp = [];
-        double _widthOffset = _treeWidth * 0.5;
-        for (var i = 0; i < str.length; i++) {
-          tmp.add(CircleAnimated(
-              str[i]["dx"] * _treeWidth - _widthOffset,
-              str[i]["dy"] * _treeHeight,
-              getColor(str[i]["begincolor"]),
-              getColor(str[i]["endcolor"]),
-              str[i]["time"],
-              str[i]["type"],
-              str[i]["pwm"]));
-        }
-        conList.add(Container(
-          width: treeWidth,
-          height: treeHeight,
-          child: Column(
-            children: tmp,
-          ),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(17),
-              border: Border.all(color: Colors.blueAccent, width: 1)),
-        ));
-      } else
-        print("contents is NULL!!!!!!!!!!!");
+    if (_contents.length > 0) {
+      for (var i = 0; i < _contents.length; i++) {
+        //print(contents[i] is String);
+        if (_contents[i].length > 0) {
+          List str = json.decode(_contents[i]);
+          List<Widget> tmp = [];
+          double _widthOffset = _treeWidth * 0.5;
+          for (var i = 0; i < str.length; i++) {
+            tmp.add(CircleAnimated(
+                str[i]["dx"] * _treeWidth - _widthOffset,
+                str[i]["dy"] * _treeHeight,
+                getColor(str[i]["begincolor"]),
+                getColor(str[i]["endcolor"]),
+                str[i]["time"],
+                str[i]["type"],
+                str[i]["pwm"]));
+          }
+          conList.add(Container(
+            width: treeWidth,
+            height: treeHeight,
+            child: Column(
+              children: tmp,
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(17),
+                border: Border.all(color: Colors.blueAccent, width: 1)),
+          ));
+        } else
+          print("contents is NULL!!!!!!!!!!!");
+      }
     }
+
     conList.add(Container(
       width: treeWidth,
       height: treeHeight,
@@ -155,7 +165,6 @@ class _HomePageState extends State<HomePage> {
           Container(
             child: FloatingActionButton(
               onPressed: () {
-                //_read();
                 Navigator.of(context).pushNamed("draw");
               },
               heroTag: "home",
