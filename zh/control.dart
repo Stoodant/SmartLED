9a91f796711f3cd7ff672e9201fdfdb0bea0e688
Stoodant <stoodant@outlook.com>
2020-08-05 23:57:57 +0800

    Control v0.2 basic function

--- zh/control.dart
+++ zh/control.dart
@@ -1,81 +1,30 @@
-class customPacket {//ver0.1
+class customPacket {//ver0.2
+  int LEDamount = 0;
   Uint8List message;
-  Uint8List message2;
+  int dataLength = 0;
+  bool error = false;
+
   customPacket() {
-    int dataLength = 14;
-    int jiaoyan = 0;
 
-    int controlData = 0x01;
-    message = Uint8List(dataLength + 8);
-    var data = ByteData.view(message.buffer);
-    int num = 0x53485955;
-    data.setUint32(0, num);
-    data.setUint8(4, ((dataLength >> 0) & 0xff));
-    data.setUint8(5, ((dataLength >> 8) & 0xff));
-    data.setUint8(8, controlData);
-    data.setUint8(9, 0x03);
-    data.setUint8(10, 0x00);
-    data.setUint8(11, 0x03);
-    data.setUint8(12, 0x00);
-    data.setUint8(13, 0xaa);
-    data.setUint8(14, 0x00);
-    data.setUint8(15, 0x00);
-    data.setUint8(16, 0x00);
-    data.setUint8(17, 0xaa);
-    data.setUint8(18, 0x00);
-    data.setUint8(19, 0x00);
-    data.setUint8(20, 0x00);
-    data.setUint8(21, 0xaa);
-    for (int i = 8; i < 8 + dataLength; i++) {
-      jiaoyan += data.getUint8(i);
-    }
-    int jiaoyan2 = 0xff - (jiaoyan & 255);
-    data.setUint8(6, jiaoyan);
-    data.setUint8(7, jiaoyan2);
-
-    int dataLength2 = 4;
-    int jiaoyan3 = 0;
-
-    int controlData2 = 0x01;
-    message2 = Uint8List(dataLength2 + 8);
-    var data2 = ByteData.view(message2.buffer);
-    int num2 = 0x53485955;
-    data2.setUint32(0, num2);
-    data2.setUint8(4, ((dataLength2 >> 0) & 0xff));
-    data2.setUint8(5, ((dataLength2 >> 8) & 0xff));
-    data2.setUint8(8, controlData2);
-    data2.setUint8(9, 0x01);
-    data2.setUint8(10, 0x01);
-    data2.setUint8(11, 0x01);
-    for (int i = 8; i < 8 + dataLength2; i++) {
-      jiaoyan3 += data2.getUint8(i);
-    }
-    int jiaoyan22 = 0xff - (jiaoyan3 & 255);
-    data2.setUint8(6, jiaoyan3);
-    data2.setUint8(7, jiaoyan22);
-
+    getAmount();//获取灯的数量
+    print("created.");
   }
 
-  Future<Uint8List> sendTCP() async {
-    Uint8List returnMessage;
+  void sendTCP() async {
+    //Uint8List returnMessage;
     InternetAddress LEDaddr = InternetAddress("192.168.4.1");
-
-
     Socket socket = await Socket.connect(LEDaddr, 6600); //TCP
-
-    print("tcp向Server发送数据");
-    socket.add(message2);
-    print("tcp向Server发送完成");
+    socket.add(message);//发送数据包
     socket.listen((datarec) {
-      returnMessage = datarec;
+      print(datarec);
     }); //TCPEND
     socket.close();
-
+  }
+  void sendUDP(){
+    InternetAddress LEDaddr = InternetAddress("192.168.4.1");
     var udpSocket = RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
-
     udpSocket.then((socket) {
-      socket.send(message, LEDaddr, 6000);
-      print("向Server发送数据");
+      socket.send(message, LEDaddr, 6000);//发送数据
 //      socket.listen((event) {
 //        if (event == RawSocketEvent.read) {
 //          var recData = socket
@@ -85,6 +34,58 @@ class customPacket {//ver0.1
 //        }
 //      });
     }); //UDPEND
-    return returnMessage;
+  }
+  bool setData(int length,Uint8List dataIn){
+    //输入长度，Uint8List类型的数组，自动拼装头部和校验部分到message
+    dataLength = length;//每个数据包数据部分长度
+    if(length > 1392) return false;//最大长度不超过1392
+    message = Uint8List(8+length);//发送的数据包包头
+    var data = ByteData.view(message.buffer);
+    int num = 0x53485955;
+    data.setUint32(0, num);//头部4字节
+    data.setUint8(4, ((dataLength >> 0) & 0xff));//长度低8位
+    data.setUint8(5, ((dataLength >> 8) & 0xff));//长度高8位
+    for(int i=0;i<length;i++){//填充数据
+      message[i+8] = dataIn[i];
+    }
+    int check = 0;//校验
+    for (int i = 8; i < 8 + dataLength; i++) {
+      check += data.getUint8(i);
+    }
+    check = check & 0xff;//低8位
+    int check2 = 0xff - check;//校验高8位
+    data.setUint8(6, check);
+    data.setUint8(7, check2);
+
+    return true;
+  }
+  void getAmount() async {//获取灯的数量，不需要手动调用，实例化时自动调用一次
+    Uint8List sendData = Uint8List(1);
+    var data = ByteData.view(sendData.buffer);
+    data.setUint8(0, 0x00);
+    setData(1, sendData);
+    Uint8List receiveData;
+    InternetAddress LEDaddr = InternetAddress("192.168.4.1");
+    try{
+      Socket socket = await Socket.connect(LEDaddr, 6600); //TCP
+      socket.add(message);//发送数据包
+      socket.listen((datarec) {//接受数据包
+        receiveData =  datarec;
+        var data = ByteData.view(receiveData.buffer);
+        if(receiveData[8]==128){
+          print(receiveData);
+          this.LEDamount = data.getUint8(11);
+          this.LEDamount = this.LEDamount << 8;
+          this.LEDamount = this.LEDamount | data.getUint8(10);
+        }
+        else{
+          LEDamount = 25565;
+        }
+      }); //TCPEND
+      socket.close();
+    }catch(e){
+      print("Link error");
+      error = true;
+    }
   }
 }
