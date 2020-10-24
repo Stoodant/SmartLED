1c8603482fdcc373ed0bba365fa8271611c36717
zzzwai <1149941442@qq.com>
2020-10-24 19:45:55 +0800

    upload new app demo

---
+++ Android_NCNN_yolov4-tiny-master/app/src/main/java/gd/hq/yolov5/FrontActivity.java
@@ -0,0 +1,27 @@
+package gd.hq.yolov5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FrontActivity extends AppCompatActivity {
+
+    private Button mBtnEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+        mBtnEdit = findViewById(R.id.button2);
+        mBtnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FrontActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
