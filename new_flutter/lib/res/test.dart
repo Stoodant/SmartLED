7299e21244049baae996b6474bf1f82a45dfb17c
Stoodant <43606410+Stoodant@users.noreply.github.com>
2021-03-15 20:40:45 +0800

    Delete new_flutter directory
    update

--- new_flutter/lib/res/test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-List showNum = [];
-
-List treeNum = [];
-
-List showData = [];
-
-List treeData = [];
--- new_flutter/lib/tools/try.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String str = "124@2135@11342@";
-  String tmp = str.substring(0, str.indexOf("2135"));
-  String tmp1 = str.substring(str.indexOf("2135")+5);
-  String res = tmp + tmp1;
-  print(tmp);
-  print(tmp1);
-  print(res);
-}
